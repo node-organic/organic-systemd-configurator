f6c0139b331acde5191afca5e54155c68084d06a
outbounder <obiwonn@gmail.com>
2018-11-04 23:44:50 +0200

    respect relative template path

--- bin/organic-systemd-configurator.js
+++ bin/organic-systemd-configurator.js
@@ -3,11 +3,16 @@
 const path = require('path')
 const Angel = require('organic-angel')
 const angel = new Angel()
+let hostCWD = process.cwd()
 process.chdir(path.resolve(__dirname, '../'))
 angel.scripts.loadScript(path.resolve(__dirname, '../scripts/install.js'), () => {
   if (!process.argv[3]) {
     angel.do('install ' + process.argv[2])
   } else {
-    angel.do('install ' + process.argv[2] + ' ' + process.argv[3])
+    let templatePath = process.argv[3]
+    if (templatePath.indexOf('/') !== 0) {
+      templatePath = path.resolve(hostCWD, templatePath)
+    }
+    angel.do('install ' + process.argv[2] + ' ' + templatePath)
   }
 })
