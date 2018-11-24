8e19649546aae3ad2e9cb0ea174dae2e27493a6c
outbounder <obiwonn@gmail.com>
2018-11-24 15:34:12 +0200

    fix cli(npx) usage and get ready for 1.0.0 release

--- bin/organic-systemd-configurator.js
+++ bin/organic-systemd-configurator.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
-
 const path = require('path')
-const exec = require('../lib/exec')
+
 let hostCWD = process.cwd()
 process.chdir(path.resolve(__dirname, '../'))
+const Angel = require('organic-angel')
+let angel = new Angel()
+require('angelscripts-install-as-daemon')(angel)
 if (!process.argv[3]) {
-  exec('npx angel install ' + process.argv[2])
+  angel.do('install ' + process.argv[2])
 } else {
   let templatePath = process.argv[3]
   if (templatePath.indexOf('/') !== 0) {
     templatePath = path.resolve(hostCWD, templatePath)
   }
-  exec('npx angel install ' + process.argv[2] + ' ' + templatePath)
+  angel.do('install ' + process.argv[2] + ' ' + templatePath)
 }
